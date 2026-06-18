f0cb3e9e15631ad706c56430d8d31f1808f4db13
ezequieljm <ezequieljm020@gmail.com>
2026-06-17 22:37:40 -0300

    Test for jenkins pipeline 3

--- runjenk.sh
+++ runjenk.sh
@@ -16,7 +16,7 @@ docker run --name jenkins-docker --rm --detach \
     --volume jenkins-docker-certs:/certs/client \
     --volume jenkins-data:/var/jenkins_home \
     --publish 2376:2376 \
-    --publish 80:80 \
+    --publish 8081:80 \
     docker:dind --storage-driver=overlay2
 
 # Wait for Docker in Docker to be ready
